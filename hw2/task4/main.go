6b0b34b37694356044199794f4dc6a4e4afc8743
Roman Danilenko <roman.danilenko@restream.ru>
2024-06-10 00:35:37 +0300

    delete previous code

--- hw2/task4/main.go
+++ hw2/task4/main.go
@@ -23,53 +23,6 @@ ________________
 Вводные данные представлены в файле dz3.json
 */
 
-//func main() {
-//	data, err := readdz.Read()
-//	if err != nil {
-//		fmt.Println("error: ", err.Error())
-//		return
-//	}
-//
-//	numstudentsByGrade := make(map[int]int, len(data.Students))
-//	studentsById := make(map[int]readdz.Students, len(data.Students))
-//	for _, student := range data.Students {
-//		studentsById[student.Id] = student
-//		numstudentsByGrade[student.Grade]++
-//	}
-//
-//	allResultByObject := make(map[int][]readdz.Results, len(data.Objects))
-//	for _, results := range data.Results {
-//		allResultByObject[results.ObjectId] = append(allResultByObject[results.ObjectId], results)
-//	}
-//
-//
-//
-//	for _, obj := range data.Objects {
-//		tbl := table.New(obj.Name, "Mean")
-//		tbl.WithHeaderSeparatorRow('-')
-//
-//		sumByGrade := make(map[int]float64, len(data.Objects)*len(data.Students))
-//
-//		resultsByObject := allResultByObject[obj.Id]
-//
-//		for _, result := range resultsByObject {
-//			student := studentsById[result.StudentId]
-//			sumByGrade[student.Grade] += float64(result.Result)
-//		}
-//
-//		var overallMean float64
-//		for grade, sum := range sumByGrade {
-//			mean := sum / float64(numstudentsByGrade[grade])
-//			overallMean += mean
-//			tbl.AddRow(grade, mean, sum)
-//		}
-//		tbl.AddRow("mean", math.Round(overallMean/3*100)/100)
-//
-//		tbl.Print()
-//		fmt.Println("-----------")
-//	}
-//}
-
 func main() {
 	data, err := readdz.Read()
 	if err != nil {
