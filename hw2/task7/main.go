1c7cd9d7a1044bc29c4783d22e954966a5fddef4
Roman Danilenko <roman.danilenko@restream.ru>
2024-06-10 23:06:18 +0300

    add filter in task7

--- hw2/task7/main.go
+++ hw2/task7/main.go
@@ -33,26 +33,19 @@ func main() {
 		objectsByID[object.Id] = object
 	}
 
-	excellentStudent := make(map[int]struct{}, len(data.Students))
-	notExcellentStudent := make(map[int]struct{}, len(data.Students))
-	for _, result := range data.Results {
-		if result.Result == 5 {
-			if _, ok := notExcellentStudent[result.StudentId]; ok {
-				continue
-			}
-			excellentStudent[result.StudentId] = struct{}{}
-		} else {
-			notExcellentStudent[result.StudentId] = struct{}{}
-			if _, ok := excellentStudent[result.StudentId]; ok {
-				delete(excellentStudent, result.StudentId)
-			}
-		}
+	notExcellentResults := filter(data.Results, func(results readdz.Results) bool {
+		return results.Result != 5
+	})
+
+	notExcellentStudent := make(map[int]struct{}, len(notExcellentResults))
+	for _, result := range notExcellentResults {
+		notExcellentStudent[result.StudentId] = struct{}{}
 	}
 
 	tbl := table.New("Student name", "Grade", "Object", "Result")
 	for _, result := range data.Results {
 		student := studentsByID[result.StudentId]
-		if _, ok := excellentStudent[student.Id]; !ok {
+		if _, ok := notExcellentStudent[student.Id]; ok {
 			continue
 		}
 
@@ -63,3 +56,62 @@ func main() {
 
 	tbl.Print()
 }
+
+func filter[T comparable](arr []T, f func(T) bool) []T {
+	res := make([]T, 0, len(arr))
+	for _, v := range arr {
+		if f(v) {
+			res = append(res, v)
+		}
+	}
+	return res
+}
+
+// without filter
+//func main() {
+//	data, err := readdz.Read()
+//	if err != nil {
+//		fmt.Println("error: ", err.Error())
+//		return
+//	}
+//
+//	studentsByID := make(map[int]readdz.Students, len(data.Students))
+//	for _, student := range data.Students {
+//		studentsByID[student.Id] = student
+//	}
+//
+//	objectsByID := make(map[int]readdz.Objects, len(data.Objects))
+//	for _, object := range data.Objects {
+//		objectsByID[object.Id] = object
+//	}
+//
+//	excellentStudent := make(map[int]struct{}, len(data.Students))
+//	notExcellentStudent := make(map[int]struct{}, len(data.Students))
+//	for _, result := range data.Results {
+//		if result.Result == 5 {
+//			if _, ok := notExcellentStudent[result.StudentId]; ok {
+//				continue
+//			}
+//			excellentStudent[result.StudentId] = struct{}{}
+//		} else {
+//			notExcellentStudent[result.StudentId] = struct{}{}
+//			if _, ok := excellentStudent[result.StudentId]; ok {
+//				delete(excellentStudent, result.StudentId)
+//			}
+//		}
+//	}
+//
+//	tbl := table.New("Student name", "Grade", "Object", "Result")
+//	for _, result := range data.Results {
+//		student := studentsByID[result.StudentId]
+//		if _, ok := excellentStudent[student.Id]; !ok {
+//			continue
+//		}
+//
+//		obj := objectsByID[result.ObjectId]
+//
+//		tbl.AddRow(student.Name, student.Grade, obj.Name, result.Result)
+//	}
+//
+//	tbl.Print()
+//}
