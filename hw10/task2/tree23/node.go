c3b2337824154927e85ca52275f4b7f7f8b217b0
Roman Danilenko <roman.danilenko@restream.ru>
2024-07-21 13:46:22 +0300

    task2

---
+++ hw10/task2/tree23/node.go
@@ -0,0 +1,93 @@
+package tree23
+
+type Node struct {
+	vals     []int
+	parent   *Node
+	children []*Node
+}
+
+func (n *Node) isLeaf() bool {
+	return len(n.children) == 0
+}
+
+func (n *Node) insertVal(val int) {
+	n.vals = append(n.vals, val)
+	n.sortVals()
+}
+
+func (n *Node) sortVals() {
+	if len(n.vals) < 2 {
+		return
+	}
+
+	if n.vals[0] > n.vals[1] {
+		n.vals[1], n.vals[0] = n.vals[0], n.vals[1]
+	}
+
+	if len(n.vals) == 3 {
+		if n.vals[2] < n.vals[1] {
+			n.vals[1], n.vals[2] = n.vals[2], n.vals[1]
+		}
+		if n.vals[0] > n.vals[1] {
+			n.vals[1], n.vals[0] = n.vals[0], n.vals[1]
+		}
+	}
+}
+
+func (n *Node) findChild(val int) *Node {
+	for i, v := range n.vals {
+		if val < v {
+			return n.children[i]
+		}
+	}
+
+	return n.children[len(n.children)-1]
+}
+
+func (n *Node) GetChildren() []*Node {
+	return n.children
+}
+
+func (n *Node) GetVals() []int {
+	return n.vals
+}
+
+func (n *Node) deleteFromLeaf(val int) {
+	for i, v := range n.vals {
+		if v == val {
+			n.vals = append(n.vals[:i], n.vals[i+1:]...)
+			break
+		}
+	}
+}
+
+func (n *Node) hasVal(val int) bool {
+	for _, v := range n.vals {
+		if v == val {
+			return true
+		}
+	}
+	return false
+}
+
+func (n *Node) replaceVal(old, new int) {
+	for i, v := range n.vals {
+		if v == old {
+			n.vals[i] = new
+			return
+		}
+	}
+}
+
+func (n *Node) getPredecessor(val int) *Node {
+	for i, v := range n.vals {
+		if v == val {
+			child := n.children[i]
+			for !child.isLeaf() {
+				child = child.children[len(child.children)-1]
+			}
+			return child
+		}
+	}
+	return nil
+}
